700e100fec1c79c7a997094f4148b1e9c051b8ac
Raphael Foulon-Binet <raphael.foulonbinet@gmail.com>
2024-01-17 17:54:27 +0100

    Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,10 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="body-content">
+    <>
       <Navbar />
       <Outlet />
-    </div>
+    </>
   );
 }
 
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,42 @@
 import Spline from "@splinetool/react-spline";
+import Button from "@mui/material/Button";
+import { Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const buttonSX = {
+  width: "10rem",
+  border: "solid",
+  borderColor: "black",
+  borderRadius: "20px",
+  transition: "transform 250ms",
+  borderWidth: "2px",
+  "&:hover": {
+    backgroundColor: "rgba(255, 255, 255, 0.15)",
+    transform: "scale(0.9)",
+  },
+};
 
 function HomePage() {
   return (
     <div className="body-content">
-      <h1> HomePage </h1>
-      <Spline scene="https://prod.spline.design/ISRA1bZLQVEKa8wW/scene.splinecode" />
+      <div className="body-center">
+        <h1 className="home-page-title">
+          L'ORÉAL met l'IA
+          <br /> au service de la beauté
+        </h1>
+        <Spline
+          scene="https://prod.spline.design/ISRA1bZLQVEKa8wW/scene.splinecode"
+          className="spline"
+        />
+      </div>
+      <div className="home-page-button">
+        <Button sx={buttonSX} component={Link} to="/beauty-ia">
+          {" "}
+          <Typography variant="h5" color="black">
+            Ask Me
+          </Typography>{" "}
+        </Button>
+      </div>
     </div>
   );
 }
