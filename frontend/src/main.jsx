b50375842eb591f0d67505fa1b87b95d66a0e58e
Hugo_Durand Tour <hugo.durand143@gmail.com>
2024-01-17 16:40:00 +0100

    Questions page V1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import Questions from "./components/Questions";
+import "./App.css";
 import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/Questions",
+    element: <Questions />,
   },
 ]);
 
