8dd71fc6f3934825823d0599762f82db1464f815
Flavien G <44913995+fgaujard@users.noreply.github.com>
2024-01-18 01:34:03 +0100

    lalalo

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -15,8 +15,9 @@ function HomePage() {
     fontSize: "medium",
     color: "#28292C",
     transition: "transform 250ms",
+    boxShadow: "0 10px 10px rgba(0, 0, 0, 0.10)",
     "&:hover": {
-      backgroundColor: "rgba(255, 255, 255, 0.15)",
+      backgroundColor: "rgba(0, 0, 0, 0.05)",
       transform: "scale(0.9)",
     },
   };
