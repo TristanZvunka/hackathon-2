ea526f8c41155304a00d7ba4241127c0f6d5f9e5
Raphael Foulon-Binet <raphael.foulonbinet@gmail.com>
2024-01-18 22:22:09 +0100

    fix

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: {
-          light: "#e6e3de",
-          DEFAULT: "#e6e3de",
-          dark: "#252525",
-        },
-        secondary: {
-          light: "#252525",
-          DEFAULT: "#252525",
-          dark: "#e6e3de",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
