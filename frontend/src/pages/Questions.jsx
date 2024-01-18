1e2bb2935e7d0cbb3560d43f8f82628342d3d646
Flavien <44913995+fgaujard@users.noreply.github.com>
2024-01-18 23:21:21 +0100

    Update Questions.jsx

--- frontend/src/pages/Questions.jsx
+++ frontend/src/pages/Questions.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
